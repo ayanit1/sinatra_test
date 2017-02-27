9746804e2e9e9b2dd80e9232151b7da557a59313
Albert <ayanit1@googlemail.com>
2017-02-27 16:07:56 +0000

    sinatra: ERB

--- app.rb
+++ app.rb
@@ -2,21 +2,6 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "My name is Albert"
-end
-
-get '/funny' do
-  "This is in the funny route"
-end
-
 get '/cat' do
-  
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
